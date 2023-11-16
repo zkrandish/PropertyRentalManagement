f858394a7542783eaeb778e32af616b188c5b2e0
Zahra Karandish <zkarandish@gmail.com>
2023-11-16 14:20:02 -0500

    manager login before adding apartment

---
+++ PropertyRentalManagementWebSite/Controllers/ManagerDashboardController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PropertyRentalManagementWebSite.Controllers
+{
+    
+    public class ManagerDashboardController : Controller
+    {
+        // GET: ManagerDashboard
+        public ActionResult Index()
+        {
+            if (Session["UserRole"] as string == "Manager")
+            {
+                return View();
+
+            }
+            return View("UnauthorizedAccess");
+        }
+        public ActionResult ManageBuildings()
+        {
+            return RedirectToAction("Index", "Buildings");
+        }
+        public ActionResult ManageApartments()
+        {
+            return RedirectToAction("Index", "Apartments");
+        }
+    }
+}
